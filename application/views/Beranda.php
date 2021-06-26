89dcf66a26e0bda3f59688af47f46cab60d79dc5
Firdrahdja <42066673+Firdrahdja@users.noreply.github.com>
2021-06-26 19:27:25 +0700

    Update Beranda.php

--- application/views/Beranda.php
+++ application/views/Beranda.php
@@ -107,7 +107,7 @@ Description:    Presentation Page
                 Di PollutInfo anda dapat menemukan informasi-informasi tentang
                 perlautan di Indonesia dan PollutInfo juga menyediakan informasi
                 singkat tentang perlautan mancanegara. Topik utama website ini adalah tentang polusi yang ada di laut Indonesia.
-				Setidaknya setiap 2 minggu sekali seluruh informasi akan di update.
+				        Setidaknya setiap 2 minggu sekali seluruh informasi akan di update.
               </p>
             </div>
           </div>
@@ -151,14 +151,14 @@ Description:    Presentation Page
     <!-- COUNT -->
     <section id="count" class="fabtheme-parallax-2 img-fluid">
       <div class="container-fluid h-100">
-        <div class="row h-100 align-items-center mt-5 justify-content-end">
-          <div class="col-11 col-sm-11 col-md-10 col-lg-10 col-xl-9 pr-0">
+        <div class="row h-100 align-items-center mt-5 justify-content-start">
+          <div class="col-11 col-sm-11 col-md-10 col-lg-10 col-xl-9 pl-0">
             <div
               class="
                 p-3 p-sm-4 p-md-4 p-lg-5 p-xl-5
-                text-right
+                text-left
                 wow
-                fadeInRight
+                fadeInLeft
                 fabtheme-count
               "
               data-wow-duration="3s"
@@ -241,13 +241,13 @@ Description:    Presentation Page
           >
             <div class="mb-5">
               <div
-                class="card-columns wow fadeInLeft fabtheme-card-columns"
+                class="card-columns wow fadeInRight fabtheme-card-columns"
                 data-wow-duration="3s"
               >
                 <div class="card fabtheme-zoom rounded-0">
                   <a href="javascript:void(0);"
                     ><img
-                      src="https://via.placeholder.com/640x426/.png"
+                      src="https://environment-indonesia.com/wp-content/uploads/2015/06/deepwater-horizon-oil-leak.jpg"
                       class="card-img mx-auto"
                       alt="All1"
                   /></a>
@@ -255,7 +255,7 @@ Description:    Presentation Page
                 <div class="card fabtheme-zoom rounded-0">
                   <a href="javascript:void(0);"
                     ><img
-                      src="https://via.placeholder.com/640x426/.png"
+                      src="http://leuserconservation.org/wp-content/uploads/2017/03/pencermaran.jpg"
                       class="card-img mx-auto"
                       alt="All2"
                   /></a>
@@ -263,20 +263,20 @@ Description:    Presentation Page
                 <div class="card fabtheme-zoom rounded-0">
                   <a href="javascript:void(0);"
                     ><img
-                      src="https://via.placeholder.com/640x426/.png"
+                      src="https://images.bisnis-cdn.com/posts/2018/12/13/869279/antarafoto-laut-indonesia-darurat-sampah-261118-da-1-1.jpg"
                       class="card-img mx-auto"
                       alt="All3"
                   /></a>
                 </div>
               </div>
               <div
-                class="card-columns wow fadeInRight fabtheme-card-columns"
+                class="card-columns wow fadeInLeft fabtheme-card-columns"
                 data-wow-duration="3s"
               >
                 <div class="card fabtheme-zoom rounded-0">
                   <a href="javascript:void(0);"
                     ><img
-                      src="https://via.placeholder.com/640x426/.png"
+                      src="https://asset-a.grid.id/crop/0x0:0x0/700x465/photo/2019/03/13/3629532859.jpg"
                       class="card-img mx-auto"
                       alt="All4"
                   /></a>
@@ -284,7 +284,7 @@ Description:    Presentation Page
                 <div class="card fabtheme-zoom rounded-0">
                   <a href="javascript:void(0);"
                     ><img
-                      src="https://via.placeholder.com/640x426/.png"
+                      src="https://assets.weforum.org/article/image/large_aDtC1oGQdRIqzmukIlDT3GFQy7EPZzng_-qIXFwFhMY.jpg"
                       class="card-img mx-auto"
                       alt="All5"
                   /></a>
@@ -292,7 +292,7 @@ Description:    Presentation Page
                 <div class="card fabtheme-zoom rounded-0">
                   <a href="javascript:void(0);"
                     ><img
-                      src="https://via.placeholder.com/640x426/.png"
+                      src="https://statik.tempo.co/data/2018/11/28/id_800216/800216_720.jpg"
                       class="card-img mx-auto"
                       alt="All6"
                   /></a>
@@ -314,7 +314,7 @@ Description:    Presentation Page
                 <div class="card fabtheme-zoom rounded-0">
                   <a href="javascript:void(0);"
                     ><img
-                      src="https://via.placeholder.com/640x426/.png"
+                      src="https://awsimages.detik.net.id/visual/2018/04/04/bfef0f4a-9025-4379-9b43-0a94ab044c8c.jpeg?w=650"
                       class="card-img mx-auto"
                       alt="Creative1"
                   /></a>
@@ -322,7 +322,7 @@ Description:    Presentation Page
                 <div class="card fabtheme-zoom rounded-0">
                   <a href="javascript:void(0);"
                     ><img
-                      src="https://via.placeholder.com/640x426/.png"
+                      src="https://indonesia.go.id/assets/upload/headline//plastiksfjbdsj.jpeg"
                       class="card-img mx-auto"
                       alt="Crative2"
                   /></a>
@@ -330,7 +330,7 @@ Description:    Presentation Page
                 <div class="card fabtheme-zoom rounded-0">
                   <a href="javascript:void(0);"
                     ><img
-                      src="https://via.placeholder.com/640x426/.png"
+                      src="https://gdb.voanews.com/D376868C-2F16-41F0-B449-2C77BF9E77DD_w1200_r1.jpg"
                       class="card-img mx-auto"
                       alt="Creative3"
                   /></a>
@@ -352,7 +352,7 @@ Description:    Presentation Page
                 <div class="card fabtheme-zoom rounded-0">
                   <a href="javascript:void(0);"
                     ><img
-                      src="https://via.placeholder.com/640x426/.png"
+                      src="https://i.pinimg.com/736x/0c/67/b3/0c67b3317c9f63b1ab9defb68b35f2b5--ocean-pollution-garage.jpg"
                       class="card-img mx-auto"
                       alt="Personal1"
                   /></a>
@@ -360,7 +360,7 @@ Description:    Presentation Page
                 <div class="card fabtheme-zoom rounded-0">
                   <a href="javascript:void(0);"
                     ><img
-                      src="https://via.placeholder.com/640x426/.png"
+                      src="https://media.nationalgeographic.org/assets/photos/199/936/966a6644-a092-4ec0-899f-f6bb377af14b.jpg"
                       class="card-img mx-auto"
                       alt="Personal2"
                   /></a>
@@ -368,7 +368,7 @@ Description:    Presentation Page
                 <div class="card fabtheme-zoom rounded-0">
                   <a href="javascript:void(0);"
                     ><img
-                      src="https://via.placeholder.com/640x426/.png"
+                      src="https://i.pinimg.com/originals/8c/e2/b2/8ce2b2056aefe2c2239a11b07da2b3e4.jpg"
                       class="card-img mx-auto"
                       alt="Personal3"
                   /></a>
@@ -436,7 +436,7 @@ Description:    Presentation Page
                           class="form-control"
                           placeholder="Pertanyaan"
                           rows="4"
-						  required="required"
+						              required="required"
                         ></textarea>
                       </div>
                       <div class="form-group fabtheme-field">
@@ -480,7 +480,7 @@ Description:    Presentation Page
                 <div class="card fabtheme-team-2">
                   <img
                     class="card-img mx-auto rounded-0"
-                    src="https://via.placeholder.com/640x426/.png"
+                    src="https://blog.hubspot.com/hubfs/youtube-editor-alternatives-1.jpg"
                     alt="member1"
                   />
                   <div class="card-body p-2 p-sm-3 p-md-3 p-lg-3 p-xl-4">
@@ -499,7 +499,7 @@ Description:    Presentation Page
                 <div class="card fabtheme-team-2">
                   <img
                     class="card-img mx-auto rounded-0"
-                    src="https://via.placeholder.com/640x426/.png"
+                    src="https://www.thoughtco.com/thmb/iFjK5a3Sndj8IMSefiiE1corABg=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/midsection-of-journalist-holding-microphones-while-writing-on-note-pad-878027776-5ae3885e04d1cf003cf1fce5.jpg"
                     alt="member2"
                   />
                   <div class="card-body p-2 p-sm-3 p-md-3 p-lg-3 p-xl-4">
@@ -518,7 +518,7 @@ Description:    Presentation Page
 				<div class="card fabtheme-team-2">
                   <img
                     class="card-img mx-auto rounded-0"
-                    src="https://via.placeholder.com/640x426/.png"
+                    src="https://img.theculturetrip.com/450x/smart/wp-content/uploads/2018/01/pexels-photo-368893-web.jpg"
                     alt="member3"
                   />
                   <div class="card-body p-2 p-sm-3 p-md-3 p-lg-3 p-xl-4">
@@ -537,7 +537,7 @@ Description:    Presentation Page
                 <div class="card fabtheme-team-2">
                   <img
                     class="card-img mx-auto rounded-0"
-                    src="https://via.placeholder.com/640x426/.png"
+                    src="https://d8qb5cxd9qhkd.cloudfront.net/ckeditor/import/52933-journalismpic.jpeg"
                     alt="member3"
                   />
                   <div class="card-body p-2 p-sm-3 p-md-3 p-lg-3 p-xl-4">
@@ -578,52 +578,61 @@ Description:    Presentation Page
               <h2>Berita Singkat Terkini</h2>
               <p class="py-2">
                 Sekumpulan rangkuman berita laut terkini yang ada di Indonesia maupun mancanegara. 
-				Berita akan diganti setiap 2 minggu sekali mengikuti sistem update website PollutInfo.
+				        Berita akan diganti setiap 2 minggu sekali mengikuti sistem update website PollutInfo.
               </p>
               <div class="fabtheme-owl-1 owl-carousel d-flex">
                 <div class="card fabtheme-blog-2">
                   <a href="javascript:void(0);">
                     <img
-                      src="https://via.placeholder.com/1920x1280/.png"
+                      src="https://earthsky.org/upl/2021/06/earth-layers-shutterstock.png"
                       class="card-img mx-auto rounded-0"
                       alt="blog1"
                     />
                   </a>
                   <div class="card-body pt-5 pr-0">
-                    <h4 class="card-title font-weight-bold">Judul Berita 1</h4>
+                    <h4 class="card-title font-weight-bold">Inti Bumi di Bawah Laut RI Miring</h4>
                     <p class="card-text pt-3">
-                      Deskripsi Berita 1
+                    Laporan mengejutkan tentang inti bumi (core) membuat heboh. Bagaimana tidak, ilmuwan telah menemukan bahwa inti bumi tumbuh miring, di bawah Indonesia, tepatnya Laut Banda.
+                    Ini terjadi akibat inti bumi di Indonesia kehilangan panas lebih cepat dari pada yang berada di wilayah lain, yakni Brasil. Akibatnya pendinginan terjadi lebih cepat akibat kristalisasi besi penyusun inti, 
+                    sebagaimana dilaporkan The Independent dikutip Jumat (18/6/2021).
+                    Yang mengejutkan lagi, hal ini ternyata sudah terjadi sejak 500 tahun lalu. Tapi belum diketahui mengapa proses pendinginan terjadi dengan cepat di RI.
+                    "Satu-satunya cara kami dapat jelaskan adalah sisi satunya tumbuh lebih cepat disbanding yang lain," kata penulis utama studi Daniel Frost, seismolog di University of California, Berkeley.
+                    Penemuan bahwa inti bumi di Indonesia kehilangan panas lebih cepat ini membawa ilmuwan pada suatu kesimpulan baru. Soal mengapa kristalisasi besi lebih condong di barat (Laut Banda) disbanding Timur (Brasil).
                     </p>
                   </div>
                 </div>
                 <div class="card fabtheme-blog-2">
                   <a href="javascript:void(0);">
                     <img
-                      src="https://via.placeholder.com/1920x1280/.png"
+                      src="https://asset.kompas.com/crops/pMqA8QP6R2jIkQ3avJAsukFIeSw=/72x0:993x614/750x500/data/photo/2021/06/06/60bc6f2ad41c8.jpeg"
                       class="card-img mx-auto rounded-0"
                       alt="blog2"
                     />
                   </a>
                   <div class="card-body pt-5 pr-0">
-                    <h4 class="card-title font-weight-bold">Judul Berita 2</h4>
+                    <h4 class="card-title font-weight-bold">Mochtar Kusumaatmadja, Bapak Hukum Laut Indonesia yang Kini Telah Tiada...</h4>
                     <p class="card-text pt-3">
-                      Deskripsi Berita 2
+                    Mantan Menteri Luar Negeri dan Menteri Kehakiman Mochtar Kusumaatmadja tutup usia di umurnya yang ke-92 tahun pada Minggu (6/6/2021). Selain dikenal karena jabatannya sebagai Menteri Luar Negeri dan menteri Kehakiman, pria kelahiran Jakarta 17 April 1929 itu juga mashyur sebagai Bapak Hukum Laut Indonesia.
+                    Pada 1958-1961, Mochtar juga mewakili Indonesia dalam Konferensi Hukum Laut, Jenewa, Colombo, dan Tokyo. Beberapa karya tulisnya juga telah mengilhami lahirnya Undang-undang Landas Kontinen Indonesia tahun 1970. Prestasi Mochtar yang diakui dunia internasional ialah kala menjadi Wakil Indonesia di Sidang PBB mengenai Hukum Laut, Jenewa dan New York.
                     </p>
                   </div>
                 </div>
                 <div class="card fabtheme-blog-2">
                   <a href="javascript:void(0);">
                     <img
-                      src="https://via.placeholder.com/1920x1280/.png"
+                      src="https://asset.kompas.com/crops/gZWlWfm8X7EsDEUZHXvq-y4J4JE=/0x0:1271x847/750x500/data/photo/2021/04/12/60742b4071f07.jpeg"
                       class="card-img mx-auto rounded-0"
                       alt="blog3"
                     />
                   </a>
                   <div class="card-body pt-5 pr-0">
-                    <h4 class="card-title font-weight-bold">Judul Berita 3</h4>
+                    <h4 class="card-title font-weight-bold">KKP Beberkan Penyebab Nelayan Vietnam Gemar Mencuri di Laut Indonesia</h4>
                     <p class="card-text pt-3">
-                      Deskripsi Berita 3
-                    </p>
+                    Direktorat Jenderal Pengawasan Sumber Daya Kelautan dan Perikanan (PSDKP) Kementerian Kelautan dan Perikanan membeberkan alasan di balik nelayan Vietnam yang sering melakukan pencurian ikan di wilayah Indonesia. 
+                    Direktur Pemantauan dan Operasi Armada Pung Nugroho Saksono menjelaskan salah satu alasannya adalah lantaran sistem pengelolaan perikanan di Vietnam yang masih belum terkontrol. 
+                    "Nelayan Vietnam sering menurunkan trol atau jaring besar untuk menangkap ikan, hasilnya memang bagus karena jaring aktif, tapi ujung-ujungnya terumbu karang jadi habis. Nah, kalau terumbu karang habis, 
+                    ikan akan lari ke tempat yang terumbu karangnya ada, makanya ikan lari dari Vietnam ke Indonesia," ujarnya dalam Podcast PSDKP: Illegal Fishing di Laut Natuna, yang disiarkan secara virtual, Rabu (5/5/2021).
+                  </p>
                   </div>
                 </div>
               </div>
@@ -656,14 +665,14 @@ Description:    Presentation Page
   <form action="<?php echo base_url()."index.php/hal_admin/recruit" ?>" method="POST">
     <section id="contact" class="fabtheme-link-show">
       <div class="container-fluid h-100">
-        <div class="row h-100 align-items-center mt-5 justify-content-end">
-          <div class="col-11 col-sm-11 col-md-10 col-lg-10 col-xl-9 pr-0">
+        <div class="row h-100 align-items-center mt-5 justify-content-start">
+          <div class="col-11 col-sm-11 col-md-10 col-lg-10 col-xl-9 pl-0">
             <div
               class="
                 p-3 p-sm-4 p-md-4 p-lg-5 p-xl-5
-                text-right
+                text-left
                 wow
-                fadeInRight
+                fadeInLeft
                 fabtheme-contact
               "
               data-wow-duration="3s"
@@ -745,14 +754,14 @@ Description:    Presentation Page
     <!-- NEWSLETTER -->
     <section id="newsletter" class="fabtheme-parallax-4 img-fluid">
       <div class="container-fluid h-100">
-        <div class="row h-100 align-items-center mt-5 justify-content-start">
-          <div class="col-11 col-sm-11 col-md-10 col-lg-10 col-xl-9 pl-0">
+        <div class="row h-100 align-items-center mt-5 justify-content-end">
+          <div class="col-11 col-sm-11 col-md-10 col-lg-10 col-xl-9 pr-0">
             <div
               class="
                 p-3 p-sm-4 p-md-4 p-lg-5 p-xl-5
-                text-left
+                text-right
                 wow
-                fadeInLeft
+                fadeInRight
                 fabtheme-newsletter
               "
               data-wow-duration="3s"
@@ -822,7 +831,7 @@ Description:    Presentation Page
               >
             </p>
             <p>
-              <a href="javascript:void(0);" class="fabtheme-link-3">Peta Laut</a>
+              <a href="https://www.kkp.go.id/" class="fabtheme-link-3">KKP</a>
             </p>
           </div>
           <div class="col-12 text-center pt-3 pt-sm-4 pt-md-4 pt-lg-5 pt-xl-5">
